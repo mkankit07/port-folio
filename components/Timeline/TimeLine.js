24c76dc723defc4269499da3dabfee664d022d1c
mkankit07 <mkankit189@gmail.com>
2023-01-15 03:18:50 +0530

    fix: build issue

--- components/Timeline/TimeLine.js
+++ components/Timeline/TimeLine.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import React, { useState, useRef, useEffect } from 'react';
-
 import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { TimeLineData } from '../../constants/constants';
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import BgAnimation from '../components/BackgroundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
+import Timeline from '../components/Timeline/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
