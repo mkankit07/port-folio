6190d725ae1a1efb6c40608c705611a02cb8df27
mkankit07 <83582606+mkankit07@users.noreply.github.com>
2023-05-10 00:47:38 +0530

    Update Hero.js

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -7,6 +7,7 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
+    const [showFullContent, setShowFullContent] = useState(false);
     <>
         <Section row nopadding>
             <LeftSection>
@@ -15,9 +16,9 @@ const Hero = (props) => (
                     I`m Ankit Maurya
                 </SectionTitle>
                 <SectionText>
-                   As a web designer and developer, I specialize in creating stylish and modern websites, web applications and online stores, serving clients of all sizes. My goal is to provide high-quality and visually appealing solutions that engage users and help businesses succeed in the o.nline space. With a focus on customer satisfaction, I work closely with clients to understand their needs and deliver results that exceed their expectations.
+                {showFullContent?"As a web designer and developer, I specialize in creating stylish and modern websites, web applications and online stores, serving clients of all sizes. My goal is to provide high-quality and visually appealing solutions that engage users and help businesses succeed in the o.nline space. With a focus on customer satisfaction, I work closely with clients to understand their needs and deliver results that exceed their expectations.":"As a web designer and developer, I specialize in creating stylish and modern websites, web applications and online stores, serving clients of all sizes. My goal is to provide high-quality and visually appealing solutions that engage users and help businesses succeed in the o.nline space."}
                 </SectionText>
-                <Button onClick={props.handleClick}>Learn More</Button>
+                <Button onClick={()=>{setShowFullContent(!showFullContent)}}>Learn More</Button>
             </LeftSection>
         </Section>
     </>
