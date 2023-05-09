f78125ce2740c408feca83b5640fc244c95c4c61
mkankit07 <83582606+mkankit07@users.noreply.github.com>
2023-05-10 00:20:45 +0530

    Add hero content

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -15,8 +15,7 @@ const Hero = (props) => (
                     I`m Ankit Maurya
                 </SectionTitle>
                 <SectionText>
-                    I design and develop services for customers of all sizes, specializing in creating stylish, modern website, web and online services
-                    stores.
+                   As a web designer and developer, I specialize in creating stylish and modern websites, web applications and online stores, serving clients of all sizes. My goal is to provide high-quality and visually appealing solutions that engage users and help businesses succeed in the o.nline space. With a focus on customer satisfaction, I work closely with clients to understand their needs and deliver results that exceed their expectations.
                 </SectionText>
                 <Button onClick={props.handleClick}>Learn More</Button>
             </LeftSection>
