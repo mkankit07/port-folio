333c951057b6bf92454d75c7961e6919decacec8
mkankit07 <83582606+mkankit07@users.noreply.github.com>
2023-05-10 00:22:27 +0530

    Update TimeLine.js

--- components/Timeline/TimeLine.js
+++ components/Timeline/TimeLine.js
@@ -44,8 +44,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      I design and develop services for customers of all sizes, specializing in creating stylish, modern website, web and online services
-    stores.     
+     As a web designer and developer, I specialize in creating stylish and modern websites, web applications and online stores, serving clients of all sizes. My goal is to provide high-quality and visually appealing solutions that engage users and help businesses succeed in the online space. With a focus on customer satisfaction, I work closely with clients to understand their needs and deliver results that exceed their expectations.     
      </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
